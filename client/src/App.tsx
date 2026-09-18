482840b0cb732e366bdc61b5d2a1456b790b8694
Holaoluwakintan <your-email@example.com>
2026-09-18 19:20:48 +0100

    Improve public community and author experiences

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home, { BookDetail, Discover, Library, NotFoundPage, Reader } from "./pages/Home";
-import Community, { AuthorPage, JournalPage, Notifications } from "./pages/Community";
+import Community, { AuthorPage, CommunityGuidelines, JournalPage, Notifications } from "./pages/Community";
 import WriterStudio from "./pages/WriterStudio";
 import ProfilePage from "./pages/ProfilePage";
 import BookStudio from "./pages/BookStudio";
@@ -12,6 +12,6 @@ import AdminBooks from "./pages/AdminBooks";
 import AdminModeration from "./pages/AdminModeration";
 
 function Router() { return <Switch>
-  <Route path="/" component={Home} /><Route path="/discover" component={Discover} /><Route path="/library" component={Library} /><Route path="/create" component={WriterStudio} /><Route path="/write" component={WriterStudio} /><Route path="/studio/books" component={BookStudio} /><Route path="/admin/books" component={AdminBooks} /><Route path="/admin/moderation" component={AdminModeration} /><Route path="/profile" component={ProfilePage} /><Route path="/community" component={Community} /><Route path="/notifications" component={Notifications} /><Route path="/authors/:slug" component={AuthorPage} /><Route path="/journal/:slug" component={JournalPage} /><Route path="/book/:id" component={BookDetail} /><Route path="/read/:id" component={Reader} /><Route component={NotFoundPage} />
+  <Route path="/" component={Home} /><Route path="/discover" component={Discover} /><Route path="/library" component={Library} /><Route path="/create" component={WriterStudio} /><Route path="/write" component={WriterStudio} /><Route path="/studio/books" component={BookStudio} /><Route path="/admin/books" component={AdminBooks} /><Route path="/admin/moderation" component={AdminModeration} /><Route path="/profile" component={ProfilePage} /><Route path="/community" component={Community} /><Route path="/community-guidelines" component={CommunityGuidelines} /><Route path="/notifications" component={Notifications} /><Route path="/authors/:slug" component={AuthorPage} /><Route path="/journal/:slug" component={JournalPage} /><Route path="/book/:id" component={BookDetail} /><Route path="/read/:id" component={Reader} /><Route component={NotFoundPage} />
 </Switch>; }
 export default function App() { return <ErrorBoundary><ThemeProvider defaultTheme="light"><TooltipProvider><Toaster position="bottom-right" /><Router /></TooltipProvider></ThemeProvider></ErrorBoundary>; }
